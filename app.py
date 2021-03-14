ba9c06d206c8dfefb2673ede3a538338f08d8da9
Paul Bennett <pbennett.uk@gmail.com>
2021-03-14 18:28:29 +0000

    adding more form features and better presenting the forms

--- app.py
+++ app.py
@@ -1,18 +1,40 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template, flash, redirect, url_for
+from forms import SignUpForm, LoginForm
 
 app = Flask(__name__)
-
-""" 
-When you're ready to set to PROD replace config.DevConfig with config.ProdConfig
-like so: app.config.from_object('config.ProdConfig')
-"""
 
 app.config.from_object("config.DevConfig")
 
 
 @app.route("/")
-def function_name():
-    return render_template("home.html")
+def home():
+    return render_template("home.html", title="Home")
+
+
+@app.route("/about")
+def second_page():
+    return render_template("about.html", title="Second Page")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == "admin@blog.com" and form.password.data == "password":
+            flash("You have been logged in!", "success")
+            return redirect(url_for("home"))
+        else:
+            flash("Login Unsuccessful. Please check username and password", "danger")
+    return render_template("login.html", title="Login", form=form)
+
+
+@app.route("/signup", methods=["GET", "POST"])
+def signup():
+    form = SignUpForm()
+    if form.validate_on_submit():
+        flash(f"Account created for {form.username.data}!", "success")
+        return redirect(url_for("home"))
+    return render_template("signup.html", title="Sign Up", form=form)
 
 
 # HTTP Error handlers ---
